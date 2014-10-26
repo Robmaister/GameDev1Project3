883623723c15b10455b5a774e0fbf31e2ced8a67
Anli Ji <anli.azuresand@gmail.com>
2014-10-26 15:49:42 -0400

    Add some scripts for TB scene

---
+++ Assets/Scripts/SelectItem1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectItem1 : MonoBehaviour {
+    ShowItemDetail otherScript;
+
+	// Use this for initialization
+	void Start () {
+        otherScript = GetComponent<ShowItemDetail>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onMouseDown() {
+        otherScript.setSelectedItem(1);
+    }
+}
---
+++ Assets/Scripts/ShowItemDetail.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowItemDetail : MonoBehaviour {
+    int itemSelected;
+
+	// Use this for initialization
+	void Start () {
+        itemSelected = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setSelectedItem(int itemNum) {
+        itemSelected = itemNum;
+    }
+
+    int getSelectedItem() {
+        return itemSelected;
+    }
+}
