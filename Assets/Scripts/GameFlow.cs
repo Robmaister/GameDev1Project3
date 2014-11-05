daa6c6d08b9f659d5851f5b0fc7e9ea4de01c330
Anli Ji <anli.azuresand@gmail.com>
2014-11-04 22:45:48 -0500

    Add gameflow script & game object

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFlow : MonoBehaviour {
+    public int day;
+    public string time;
+	// Use this for initialization
+	void Start () {
+        day = 1;
+        time = "night";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void triggerNextDay(){
+        day++;
+        time = "day";
+    }
+}
